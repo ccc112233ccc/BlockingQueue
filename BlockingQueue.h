07bd3258446f38ffda26f027cc8f845f6e15b2a6
cc <cczjum@outlook.com>
2024-04-28 14:48:01 +0800

    使用typedef重命名类型信息

--- BlockingQueue.h
+++ BlockingQueue.h
@@ -5,40 +5,42 @@
 #include <mutex>
 
 namespace ccutil {
-using std::condition_variable;
-using std::deque;
-using std::lock_guard;
-using std::mutex;
-using std::unique_lock;
+
 template <typename T>
 class BlockingQueue {
+  typedef std::deque<T> Deque;
+  typedef std::mutex mutex;
+  typedef std::lock_guard<mutex> LockGuard;
+  typedef std::unique_lock<mutex> UniqueLock;
+  typedef std::condition_variable condition_variable;
+
  public:
   BlockingQueue() = default;
 
   void put(T&& x) {
     {
-      lock_guard<mutex> lock(mutex_);
+      LockGuard lock(mutex_);
       queue_.push_back(std::move(x));
     }
     not_empty_.notify_one();
   }
 
   T take() {
-    unique_lock<mutex> lock(mutex_);
+    UniqueLock lock(mutex_);
     not_empty_.wait(lock, [this] { return !queue_.empty(); });
     T front(std::move(queue_.front()));
     queue_.pop_front();
     return front;
   }
 
-  size_t Size() const {
-    lock_guard<mutex> lock(mutex_);
+  size_t size() const {
+    LockGuard lock(mutex_);
     return queue_.size();
   }
 
  private:
   mutable mutex mutex_;
   condition_variable not_empty_;
-  deque<T> queue_;
+  Deque queue_;
 };
 }  // namespace ccutil
